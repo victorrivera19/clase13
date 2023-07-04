8a629dda09c13fa1f27f800af5f89e253ae86761
victorrivera19 <victorriver20@gmail.com>
2023-07-04 00:43:10 -0500

    inicial

---
+++ app/src/main/java/com/example/sesion13/confi/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.sesion13.confi
+
+object AppModule {
+}
---
+++ app/src/main/java/com/example/sesion13/confi/FirebaseModule.kt
@@ -0,0 +1,8 @@
+package com.example.sesion13.confi
+
+//import com.google.firebase.auth.FirebaseAuth
+
+object FirebaseModule {
+    //@Provides
+    //@Singleton7
+}
